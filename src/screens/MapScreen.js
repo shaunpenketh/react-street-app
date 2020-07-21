7dfcef7bd3759b67741e3852560073aad91323b2
Shaun Penketh <penky332@gmail.com>
2020-07-21 21:52:22 +0100

    added map view and geolocation services

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 const styles = {
-  main: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  main: {height: '100%', width: '100%'},
 };
 
 const MapScreen = ({navigation}) => {
   const {main} = styles;
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  useEffect(() => {
+    (async () => {
+      Geolocation.getCurrentPosition((info) =>
+        setLocation({
+          latitude: info.latitude,
+          longitude: info.longitude,
+        }),
+      );
+    })();
+  }, []);
 
   return (
-    <View style={main}>
-      <Text>Map Screen</Text>
-    </View>
+    <MapView
+      style={main}
+      showsUserLocation
+      initialRegion={{
+        latitude: -6.270565,
+        longitude: 106.75955,
+        latitudeDelta: 1,
+        longitudeDelta: 1,
+      }}
+    />
   );
 };
 
